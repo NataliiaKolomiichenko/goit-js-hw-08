b4a2ce60466821c0f2b7d0966871c17bce1d6844
NataliiaKolomiichenko <gella1996nr@gmail.com>
2022-07-26 00:36:06 +0300

    Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const VIDEOPLAYER_CURRENT_TIME = "videoplayer-current-time";
+
+    const player = new Vimeo(iframe);
+
+    player.on('timeupdate', throttle(saveVideoTime, 1000));
+
+function saveVideoTime(event) {
+    const currentSeconds = event.seconds;
+    localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, currentSeconds);
+}
+
+player.setCurrentTime(localStorage.getItem(VIDEOPLAYER_CURRENT_TIME)).then(function(seconds) {
+        seconds = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            if (seconds < 0 || seconds > 571.543) { return }
+            break;
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
 
+const feedbackFormEl = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+
+fillInForm();
+
+feedbackFormEl.addEventListener('input', throttle(onTextInput, 1000));
+
+feedbackFormEl.addEventListener('submit', onFormSubmit);
+
+function onTextInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+}
+
+function fillInForm() {
+    const savedText = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedText) {
+        const emailEl = document.querySelector('[type="email"]');
+        const textareaEl = document.querySelector('[name="message"]');
+        emailEl.value = savedText.email === true ? savedText.email : "";
+        textareaEl.value = savedText.message;
+    }    
+};
